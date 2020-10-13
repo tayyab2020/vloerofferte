b4659c4f327bd42b8499b818b9a52ac9d2ccb564
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2020-10-13 22:58:36 +0500

    Quotation module

--- resources/views/admin/user/quote_request_mail.blade.php
+++ resources/views/admin/user/quote_request_mail.blade.php
@@ -3,7 +3,7 @@
 <body>
 
 <p>
-    Dear Mr/Mrs {{$username}}<br><br>Your quotation request has been submitted successfully. You can go to your dashboard through <a href='{{$link}}'>here.</a><br><br>Kind regards,<br><br>Klantenservice Topstoffeerders
+    Dear Mr/Mrs {{$username}}<br><br>You have received a quotation request. Client is waiting for your response. You can go to your dashboard through <a href='{{$link}}'>here.</a><br><br>Kind regards,<br><br>Klantenservice Topstoffeerders
 </p>
 
 </body>
