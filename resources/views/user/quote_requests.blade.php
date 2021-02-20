45637c43dbc77727568d8800e24c7b442e71de56
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-02-20 14:31:16 +0500

    updates

--- resources/views/user/quote_requests.blade.php
+++ resources/views/user/quote_requests.blade.php
@@ -362,7 +362,7 @@
 
     <script type="text/javascript">
         $('#example').DataTable({
-            order: [[0, 'desc']],
+            order: [[5, 'desc']],
             "oLanguage": {
                 "sLengthMenu": "<?php echo __('text.Show') . ' _MENU_ ' . __('text.records'); ?>",
                 "sSearch": "<?php echo __('text.Search') . ':' ?>",
