f0b16ae2e9adbfa674852ea85af34a4007bbe8dd
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-03-11 01:25:43 +0500

    updates

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -14,7 +14,7 @@ class ProductsExport implements FromCollection,WithHeadings
 
     public function headings(): array
     {
-        return ["Article ID", "DB ID", "Title", "Slug", "Model number", "Size", "Measure", "Estimated price", "Additional Info", "Floor type", "Floor type 2", "Supplier", "Color", "Description", "Category", "Brand", "Model"];
+        return ["Article ID", "DB ID", "Title", "Slug", "Category", "Brand", "Model", "Model number", "Size", "Measure", "Estimated price", "Additional Info", "Floor type", "Floor type 2", "Supplier", "Color", "Description"];
     }
 
     public function collection()
@@ -40,6 +40,6 @@ class ProductsExport implements FromCollection,WithHeadings
             $key->save();
         }
 
-        return Products::leftjoin('categories','categories.id','=','products.category_id')->leftjoin('brands','brands.id','=','products.brand_id')->leftjoin('models','models.id','=','products.model_id')->select('products.article_code','products.id','products.title','products.slug','products.model_number','products.size','products.measure','products.estimated_price','products.additional_info','products.floor_type','products.floor_type2','products.supplier','products.color','products.description','categories.cat_name','brands.cat_name as brand_name','models.cat_name as model_name')->orderBy('products.article_code','Asc')->get();
+        return Products::leftjoin('categories','categories.id','=','products.category_id')->leftjoin('brands','brands.id','=','products.brand_id')->leftjoin('models','models.id','=','products.model_id')->select('products.article_code','products.id','products.title','products.slug','categories.cat_name','brands.cat_name as brand_name','models.cat_name as model_name','products.model_number','products.size','products.measure','products.estimated_price','products.additional_info','products.floor_type','products.floor_type2','products.supplier','products.color','products.description')->orderBy('products.article_code','Asc')->get();
     }
 }
--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -64,43 +64,43 @@ class ProductsImport implements ToModel, WithStartRow
     {
         if($row[1])
         {
-            $category = Category::where('cat_name',$row[5])->first();
+            $category = Category::where('cat_name',$row[4])->first();
 
             if(!$category)
             {
-                $cat_slug = Str::slug($row[5], "-");
+                $cat_slug = Str::slug($row[4], "-");
 
                 if (Category::where('cat_slug',$cat_slug)->exists()) {
                     $cat_slug = $this->incrementSlug($cat_slug,1);
                 }
 
                 $category = new Category;
-                $category->cat_name = $row[5];
+                $category->cat_name = $row[4];
                 $category->cat_slug = $cat_slug;
                 $category->save();
             }
 
-            $brand = Brand::where('cat_name',$row[6])->first();
+            $brand = Brand::where('cat_name',$row[5])->first();
 
             if(!$brand)
             {
-                $brand_slug = Str::slug($row[6], "-");
+                $brand_slug = Str::slug($row[5], "-");
 
                 if (Brand::where('cat_slug',$brand_slug)->exists()) {
                     $brand_slug = $this->incrementSlug($brand_slug,2);
                 }
 
                 $brand = new Brand;
-                $brand->cat_name = $row[6];
+                $brand->cat_name = $row[5];
                 $brand->cat_slug = $brand_slug;
                 $brand->save();
             }
 
-            $model = Model1::where('cat_name',$row[7])->first();
+            $model = Model1::where('cat_name',$row[6])->first();
 
             if(!$model)
             {
-                $model_slug = Str::slug($row[7], "-");
+                $model_slug = Str::slug($row[6], "-");
 
                 if (Model1::where('cat_slug',$model_slug)->exists()) {
                     $model_slug = $this->incrementSlug($model_slug,3);
@@ -108,7 +108,7 @@ class ProductsImport implements ToModel, WithStartRow
 
                 $model = new Model1;
                 $model->brand_id = $brand->id;
-                $model->cat_name = $row[7];
+                $model->cat_name = $row[6];
                 $model->cat_slug = $model_slug;
                 $model->save();
             }
@@ -117,7 +117,7 @@ class ProductsImport implements ToModel, WithStartRow
 
             if(!$check)
             {
-                $check1 = Products::leftjoin('categories', 'categories.id', '=', 'products.category_id')->leftjoin('brands', 'brands.id', '=', 'products.brand_id')->leftjoin('models', 'models.id', '=', 'products.model_id')->where('products.title', 'LIKE', '%' . $row[2] . '%')->where('categories.cat_name', $row[5])->where('brands.cat_name', $row[6])->where('models.cat_name', $row[7])->select('products.*')->first();
+                $check1 = Products::leftjoin('categories', 'categories.id', '=', 'products.category_id')->leftjoin('brands', 'brands.id', '=', 'products.brand_id')->leftjoin('models', 'models.id', '=', 'products.model_id')->where('products.title', 'LIKE', '%' . $row[2] . '%')->where('categories.cat_name', $row[4])->where('brands.cat_name', $row[5])->where('models.cat_name', $row[6])->select('products.*')->first();
 
                 if(!$check1)
                 {
@@ -125,19 +125,19 @@ class ProductsImport implements ToModel, WithStartRow
                     $check->article_code = $row[0];
                     $check->title = $row[2];
                     $check->slug = $row[3];
-                    $check->description = $row[4];
-                    $check->model_number = $row[8];
-                    $check->size = $row[9];
-                    $check->measure = $row[10];
-                    $check->estimated_price = $row[11];
-                    $check->additional_info = $row[12];
-                    $check->floor_type = $row[13];
-                    $check->floor_type2 = $row[14];
-                    $check->supplier = $row[15];
-                    $check->color = $row[16];
                     $check->category_id = $category->id;
                     $check->brand_id = $brand->id;
                     $check->model_id = $model->id;
+                    $check->model_number = $row[7];
+                    $check->size = $row[8];
+                    $check->measure = $row[9];
+                    $check->estimated_price = $row[10];
+                    $check->additional_info = $row[11];
+                    $check->floor_type = $row[12];
+                    $check->floor_type2 = $row[13];
+                    $check->supplier = $row[14];
+                    $check->color = $row[15];
+                    $check->description = $row[16];
                     $check->excel = 1;
                     $check->save();
 
@@ -147,16 +147,16 @@ class ProductsImport implements ToModel, WithStartRow
                 {
                     $check1->article_code = $row[0];
                     $check1->slug = $row[3];
-                    $check1->description = $row[4];
-                    $check1->model_number = $row[8];
-                    $check1->size = $row[9];
-                    $check1->measure = $row[10];
-                    $check1->estimated_price = $row[11];
-                    $check1->additional_info = $row[12];
-                    $check1->floor_type = $row[13];
-                    $check1->floor_type2 = $row[14];
-                    $check1->supplier = $row[15];
-                    $check1->color = $row[16];
+                    $check1->model_number = $row[7];
+                    $check1->size = $row[8];
+                    $check1->measure = $row[9];
+                    $check1->estimated_price = $row[10];
+                    $check1->additional_info = $row[11];
+                    $check1->floor_type = $row[12];
+                    $check1->floor_type2 = $row[13];
+                    $check1->supplier = $row[14];
+                    $check1->color = $row[15];
+                    $check1->description = $row[16];
                     $check1->excel = 1;
                     $check1->save();
 
@@ -168,19 +168,19 @@ class ProductsImport implements ToModel, WithStartRow
                 $check->article_code = $row[0];
                 $check->title = $row[2];
                 $check->slug = $row[3];
-                $check->description = $row[4];
-                $check->model_number = $row[8];
-                $check->size = $row[9];
-                $check->measure = $row[10];
-                $check->estimated_price = $row[11];
-                $check->additional_info = $row[12];
-                $check->floor_type = $row[13];
-                $check->floor_type2 = $row[14];
-                $check->supplier = $row[15];
-                $check->color = $row[16];
                 $check->category_id = $category->id;
                 $check->brand_id = $brand->id;
                 $check->model_id = $model->id;
+                $check->model_number = $row[7];
+                $check->size = $row[8];
+                $check->measure = $row[9];
+                $check->estimated_price = $row[10];
+                $check->additional_info = $row[11];
+                $check->floor_type = $row[12];
+                $check->floor_type2 = $row[13];
+                $check->supplier = $row[14];
+                $check->color = $row[15];
+                $check->description = $row[16];
                 $check->excel = 1;
                 $check->save();
 
@@ -190,61 +190,61 @@ class ProductsImport implements ToModel, WithStartRow
         }
         else {
 
-            $check = Products::leftjoin('categories', 'categories.id', '=', 'products.category_id')->leftjoin('brands', 'brands.id', '=', 'products.brand_id')->leftjoin('models', 'models.id', '=', 'products.model_id')->where('products.title', 'LIKE', '%' . $row[2] . '%')->where('categories.cat_name', $row[5])->where('brands.cat_name', $row[6])->where('models.cat_name', $row[7])->select('products.*')->first();
+            $check = Products::leftjoin('categories', 'categories.id', '=', 'products.category_id')->leftjoin('brands', 'brands.id', '=', 'products.brand_id')->leftjoin('models', 'models.id', '=', 'products.model_id')->where('products.title', 'LIKE', '%' . $row[2] . '%')->where('categories.cat_name', $row[4])->where('brands.cat_name', $row[5])->where('models.cat_name', $row[6])->select('products.*')->first();
 
             if ($check) {
 
                 $check->article_code = $row[0];
                 $check->slug = $row[3];
-                $check->description = $row[4];
-                $check->model_number = $row[8];
-                $check->size = $row[9];
-                $check->measure = $row[10];
-                $check->estimated_price = $row[11];
-                $check->additional_info = $row[12];
-                $check->floor_type = $row[13];
-                $check->floor_type2 = $row[14];
-                $check->supplier = $row[15];
-                $check->color = $row[16];
+                $check->model_number = $row[7];
+                $check->size = $row[8];
+                $check->measure = $row[9];
+                $check->estimated_price = $row[10];
+                $check->additional_info = $row[11];
+                $check->floor_type = $row[12];
+                $check->floor_type2 = $row[13];
+                $check->supplier = $row[14];
+                $check->color = $row[15];
+                $check->description = $row[16];
                 $check->excel = 1;
                 $check->save();
 
             } else {
 
-                $category = Category::where('cat_name', $row[5])->first();
+                $category = Category::where('cat_name', $row[4])->first();
 
                 if (!$category) {
-                    $cat_slug = Str::slug($row[5], "-");
+                    $cat_slug = Str::slug($row[4], "-");
 
                     if (Category::where('cat_slug', $cat_slug)->exists()) {
                         $cat_slug = $this->incrementSlug($cat_slug, 1);
                     }
 
                     $category = new Category;
-                    $category->cat_name = $row[5];
+                    $category->cat_name = $row[4];
                     $category->cat_slug = $cat_slug;
                     $category->save();
                 }
 
-                $brand = Brand::where('cat_name', $row[6])->first();
+                $brand = Brand::where('cat_name', $row[5])->first();
 
                 if (!$brand) {
-                    $brand_slug = Str::slug($row[6], "-");
+                    $brand_slug = Str::slug($row[5], "-");
 
                     if (Brand::where('cat_slug', $brand_slug)->exists()) {
                         $brand_slug = $this->incrementSlug($brand_slug, 2);
                     }
 
                     $brand = new Brand;
-                    $brand->cat_name = $row[6];
+                    $brand->cat_name = $row[5];
                     $brand->cat_slug = $brand_slug;
                     $brand->save();
                 }
 
-                $model = Model1::where('cat_name', $row[7])->first();
+                $model = Model1::where('cat_name', $row[6])->first();
 
                 if (!$model) {
-                    $model_slug = Str::slug($row[7], "-");
+                    $model_slug = Str::slug($row[6], "-");
 
                     if (Model1::where('cat_slug', $model_slug)->exists()) {
                         $model_slug = $this->incrementSlug($model_slug, 3);
@@ -252,7 +252,7 @@ class ProductsImport implements ToModel, WithStartRow
 
                     $model = new Model1;
                     $model->brand_id = $brand->id;
-                    $model->cat_name = $row[7];
+                    $model->cat_name = $row[6];
                     $model->cat_slug = $model_slug;
                     $model->save();
                 }
@@ -261,19 +261,19 @@ class ProductsImport implements ToModel, WithStartRow
                 $product->article_code = $row[0];
                 $product->title = $row[2];
                 $product->slug = $row[3];
-                $product->model_number = $row[8];
-                $product->size = $row[9];
-                $product->measure = $row[10];
-                $product->estimated_price = $row[11];
-                $product->additional_info = $row[12];
-                $product->floor_type = $row[13];
-                $product->floor_type2 = $row[14];
-                $product->supplier = $row[15];
-                $product->color = $row[16];
                 $product->category_id = $category->id;
                 $product->brand_id = $brand->id;
                 $product->model_id = $model->id;
-                $product->description = $row[4];
+                $product->model_number = $row[7];
+                $product->size = $row[8];
+                $product->measure = $row[9];
+                $product->estimated_price = $row[10];
+                $product->additional_info = $row[11];
+                $product->floor_type = $row[12];
+                $product->floor_type2 = $row[13];
+                $product->supplier = $row[14];
+                $product->color = $row[15];
+                $product->description = $row[16];
                 $product->excel = 1;
                 $product->save();
 
