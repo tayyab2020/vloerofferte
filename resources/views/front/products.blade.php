be7691276247cad933a7d654c56ce58a69a79f17
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-04-03 01:24:54 +0500

    updates

--- resources/views/front/products.blade.php
+++ resources/views/front/products.blade.php
@@ -87,8 +87,17 @@
                         <span id="slider-lowest" style="float: left;margin-top: 10px;">€ {{$lowest}}</span>
                         <span id="slider-highest" style="float: right;margin-top: 10px;">€ {{$highest}}</span>
 
-                        <input type="hidden" name="range_start" id="range-start" value="{{$s == '' && (float)$s != 0.00 ? $lowest : $s}}">
-                        <input type="hidden" name="range_end" id="range-end" value="{{$e == '' && (float)$e != 0.00 ? $highest : $e}}">
+                        @if($s == '' && $s != 0.00 && $s != 0)
+                            <input type="hidden" name="range_start" id="range-start" value="{{$lowest}}">
+                        @else
+                            <input type="hidden" name="range_start" id="range-start" value="{{$s}}">
+                        @endif
+
+                        @if($e == '' && $e != 0.00 && $e != 0)
+                            <input type="hidden" name="range_end" id="range-end" value="{{$highest}}">
+                        @else
+                            <input type="hidden" name="range_end" id="range-end" value="{{$e}}">
+                        @endif
 
                     </div> <!-- cd-filter-content -->
                 </div> <!-- cd-filter-block -->
