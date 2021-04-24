f371f9c02f0a79e24194d1fa7da166203c40acb2
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2021-04-24 21:06:24 +0500

    updates

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -1408,7 +1408,9 @@ class FrontendController extends Controller
             $s = floatval($range_s);
             $e = floatval($range_e);
 
-            $all_products = $all_products->where('estimated_prices.price','>=',$s)->where('estimated_prices.price','<=',$e);
+            $all_products = $all_products->where(function($query) use($s, $e) {
+                $query->where('estimated_prices.price','>=',$s)->where('estimated_prices.price','<=',$e);
+            })->orWhere('estimated_prices.price',NULL);
 
             $lowest = $request->org_range_start;
             $highest = $request->org_range_end;
@@ -1417,6 +1419,16 @@ class FrontendController extends Controller
         {
             $highest = estimated_prices::max('price');
             $lowest = estimated_prices::min('price');
+
+            if(!$lowest)
+            {
+                $lowest = 0;
+            }
+
+            if(!$highest)
+            {
+                $highest = 0.1;
+            }
 
             $s = $lowest;
             $e = $highest;
