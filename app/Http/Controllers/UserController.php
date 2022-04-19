0b75f5d072d0aa9056a9eb49d6c849abff7397f2
Muhammad Tayyab <tayyabkhurram62@gmail.com>
2022-04-20 02:03:47 +0500

    Updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -694,6 +694,7 @@ class UserController extends Controller
                     'currency' => 'EUR',
                     'value' => $total_mollie, // You must send the correct number of decimals, thus we enforce the use of strings
                 ],
+                'testmode' => true,
                 'description' => $description,
                 'webhookUrl' => route('webhooks.quotation_payment'),
                 'redirectUrl' => url('aanbieder/quotation-payment-redirect-page/' . $inv_encrypt),
